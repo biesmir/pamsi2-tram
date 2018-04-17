43b41db5d4d81a0882fa3471ff80b7bce4863351
Jakub Cebulski <235773@student.pwr.edu.pl>
2018-04-17 12:33:53 +0200

    edited bfs

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -9,24 +9,29 @@
 #include "../inc/graph.hh"
 
 std::shared_ptr< std::queue<int> > graph::bfs(int _ID_source, int _ID_end){
-	std::shared_ptr< std::queue<int> > route;
+	std::shared_ptr< std::queue<int> > route = new std::queue<int>;
+
 	int u;
-	std::cout<<"ok"<<std::endl;
-	//route->push(_ID_source);
-	std::cout<<"ok"<<std::endl;
+
+	route->push(_ID_source);
+/*
 	std::queue<int> Q;
-	std::cout<<"ok"<<std::endl;
 	Q.push(_ID_source);
-std::cout<<"ok"<<std::endl;
+
 	while(!Q.empty()){
-			u = Q.front();
+
+		u = Q.front();
 			for(int i=0;i<=this->stops[u]->get_number_of_connections()-1;i++){
 
 				Q.push(this->stops[u]->get_connection_ID(i));
 				std::cout<<this->stops[u]->get_connection_ID(i)<<std::endl;
 				sleep(1);
 			}
-			}
+			}*/
+
+	while(u != _ID_end){
+
+	}
 
 	return route;
 }
